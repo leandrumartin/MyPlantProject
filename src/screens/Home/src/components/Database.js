5f151aed1df0bd43d5a6d835b2b4114a04ff16f8
Leandru Martin <leandru.martin@slu.edu>
2023-07-04 14:53:13 -0500

    Improved database form UI and error messages

--- src/screens/Home/src/components/Database.js
+++ src/screens/Home/src/components/Database.js
@@ -7,10 +7,10 @@ import {
   TextInput,
   Button,
 } from 'react-native';
-import { object, string, number } from 'yup';
+import { object, string, number, setLocale } from 'yup';
 
 import * as SQLite from 'expo-sqlite';
-const db = SQLite.openDatabase('db.testDb'); // returns Database object
+const db = SQLite.openDatabase('db.plantDb'); // returns Database object
 
 export default class Database extends React.Component {
   constructor(props) {
@@ -25,21 +25,24 @@ export default class Database extends React.Component {
       validationError: null,
     };
 
-    // Check if the items table exists if not create it
+    // Check if the plants table exists if not create it
     db.transaction((tx) => {
       tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, LowerLimit INT, UpperLimit INT, RecLowerLimit INT, RecUpperLimit INT)'
+        'CREATE TABLE IF NOT EXISTS plants (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, LowerLimit INT, UpperLimit INT, RecLowerLimit INT, RecUpperLimit INT)'
       );
     });
 
     this.fetchData();
   }
 
+  /**
+   * Fetches data from database and stores it in state.
+   */
   fetchData = () => {
     db.transaction((tx) => {
       // sending 4 arguments in executeSql
       tx.executeSql(
-        'SELECT * FROM items',
+        'SELECT * FROM plants',
         null, // passing sql query and parameters:null
         // success callback which sends two things Transaction object and ResultSet Object
         (txObj, { rows: { _array } }) => {
@@ -52,22 +55,9 @@ export default class Database extends React.Component {
     }); // end transaction
   };
 
-  handleChangeName = (newData) => {
-    this.setState({ name: newData });
-  };
-  handleChangeLowerLimit = (newData) => {
-    this.setState({ lowerLimit: newData });
-  };
-  handleChangeUpperLimit = (newData) => {
-    this.setState({ upperLimit: newData });
-  };
-  handleChangeRecLowerLimit = (newData) => {
-    this.setState({ recLowerLimit: newData });
-  };
-  handleChangeRecUpperLimit = (newData) => {
-    this.setState({ recUpperLimit: newData });
-  };
-
+  /**
+   * Validates form input using rules in mySchema. Inserts into plants table if successful. Otherwise, sets this.state.validationError.
+   */
   handleSubmit = async () => {
     await mySchema
       .validate(this.state)
@@ -75,7 +65,7 @@ export default class Database extends React.Component {
         this.setState({ ...this.state, validationError: null });
         db.transaction((tx) => {
           tx.executeSql(
-            'INSERT INTO items (Name, LowerLimit, UpperLimit, RecLowerLimit, RecUpperLimit) VALUES (?, ?, ?, ?, ?)',
+            'INSERT INTO plants (Name, LowerLimit, UpperLimit, RecLowerLimit, RecUpperLimit) VALUES (?, ?, ?, ?, ?)',
             [
               this.state.name,
               this.state.lowerLimit,
@@ -96,7 +86,7 @@ export default class Database extends React.Component {
 
   handleClear = () => {
     db.transaction((tx) => {
-      tx.executeSql('DELETE FROM items');
+      tx.executeSql('DELETE FROM plants');
     });
     this.setState({
       data: null,
@@ -111,38 +101,38 @@ export default class Database extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text>Plant Name</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Plant Name"
-          onChangeText={this.handleChangeName}
-          // value={}
+          onChangeText={(newData) => this.setState({ name: newData })}
         />
+        <Text>Temperature Lower Limit</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Temperature Lower Limit"
-          onChangeText={this.handleChangeLowerLimit}
-          // value={}
+          onChangeText={(newData) => this.setState({ lowerLimit: newData })}
         />
+        <Text>Temperature Upper Limit</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Temperature Upper Limit"
-          onChangeText={this.handleChangeUpperLimit}
-          // value={}
+          onChangeText={(newData) => this.setState({ upperLimit: newData })}
         />
+        <Text>Temperature Recommended Lower Limit</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Temperature Recommended Lower Limit"
-          onChangeText={this.handleChangeRecLowerLimit}
-          // value={}
+          onChangeText={(newData) => this.setState({ recLowerLimit: newData })}
         />
+        <Text>Temperature Recommended Upper Limit</Text>
         <TextInput
           style={styles.textInput}
           placeholder="Temperature Recommended Upper Limit"
-          onChangeText={this.handleChangeRecUpperLimit}
-          // value={}
+          onChangeText={(newData) => this.setState({ recUpperLimit: newData })}
         />
 
-        <Text>
+        <Text style={styles.errorText}>
           {this.state.validationError !== null
             ? this.state.validationError.message
             : ''}
@@ -150,7 +140,7 @@ export default class Database extends React.Component {
 
         <Button
           type="solid"
-          title="Submit"
+          title="Add new plant"
           onPress={this.handleSubmit}
         ></Button>
 
@@ -164,32 +154,57 @@ export default class Database extends React.Component {
   }
 }
 
+// Set shared error messages
+setLocale({
+  mixed: {
+    required: 'All fields are required',
+  },
+});
+
+// Validation schema for Yup
 const mySchema = object({
   name: string().required(),
-  lowerLimit: number().required(),
-  upperLimit: number().required(),
+  lowerLimit: number().required().typeError('Lower limit must be a number'),
+  upperLimit: number().required().typeError('Upper limit must be a number'),
   recLowerLimit: number()
     .required()
     .when(
       'lowerLimit',
-      (lowerLimit, recLowerLimit) => lowerLimit && recLowerLimit.min(lowerLimit)
+      (lowerLimit, recLowerLimit) =>
+        lowerLimit &&
+        recLowerLimit.min(
+          lowerLimit,
+          'Recommended lower limit must be greater than lower limit'
+        )
     )
     .when(
       'recUpperLimit',
       (recUpperLimit, recLowerLimit) =>
-        recUpperLimit && recLowerLimit.max(recUpperLimit)
-    ),
+        recUpperLimit &&
+        recLowerLimit.max(
+          recUpperLimit,
+          'Recommended lower limit must be less than recommended upper limit'
+        )
+    )
+    .typeError('Recommended lower limit must be a number'),
   recUpperLimit: number()
     .required()
     .when(
       'upperLimit',
-      (upperLimit, recUpperLimit) => upperLimit && recUpperLimit.max(upperLimit)
-    ),
+      (upperLimit, recUpperLimit) =>
+        upperLimit &&
+        recUpperLimit.max(
+          upperLimit,
+          'Recommended upper limit must be less than upper limit'
+        )
+    )
+    .typeError('Recommended upper limit must be a number'),
 });
 
 const styles = {
   container: {
     flex: 1,
+    marginTop: 20,
   },
   textInput: {
     height: 40,
@@ -197,5 +212,9 @@ const styles = {
     padding: 2,
     borderWidth: 2,
     minWidth: 300,
+  },
+  errorText: {
+    color: 'red',
+    maxWidth: 300,
   },
 };
